8319d4bd68aa3644f98f32361121ac2110efc263
Sultan Mohammed <sultanaljrboua@gmail.com>
2025-05-24 01:48:25 +0300

    update

--- flask_esp32_predictor.py
+++ flask_esp32_predictor.py
@@ -9,7 +9,7 @@ model = joblib.load("lettuce_harvest_predictor.pkl")
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 def predict():
     data = request.get_json()
     try:
